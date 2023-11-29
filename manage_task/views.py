b097b35964e9249e5d07f418f67c79a95d8b33da
NicDs <nikolasdurango@gmail.com>
2023-11-28 22:06:52 -0500

    created CRUD for Task model

--- manage_task/views.py
+++ manage_task/views.py
@@ -3,6 +3,9 @@ from rest_framework.response import Response
 from .models import Person, Task
 from .serializers import PersonSerializer, TaskSerializer
 
+"""
+    CRUD for Person model
+"""
 class PersonListAPIView(generics.ListAPIView):
     serializer_class = PersonSerializer
 
@@ -35,3 +38,40 @@ class PersonDestroyAPIView(generics.DestroyAPIView):
             return Response({'message': 'Person deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
         except Person.DoesNotExist:
             return Response({'message': 'Person not found'}, status=status.HTTP_404_NOT_FOUND)
+
+"""
+    CRUD for Task model
+"""
+
+class TaskListAPIView(generics.ListAPIView):
+    serializer_class = TaskSerializer
+
+    def get_queryset(self):
+        return Task.objects.all()
+    
+class TaskRetrieveAPIView(generics.RetrieveAPIView):
+    serializer_class = TaskSerializer
+    
+    def get_queryset(self):
+        return Task.objects.all()
+    
+class TaskCreateAPIView(generics.CreateAPIView):
+    serializer_class = TaskSerializer
+
+    def post(self, request):
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message': 'Task created successfully'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class TaskDestroyAPIView(generics.DestroyAPIView):
+    serializer_class = TaskSerializer
+    
+    def destroy(self, request, pk):
+        try:
+            task = Task.objects.get(pk=pk)
+            task.delete()
+            return Response({'message': 'Task deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
+        except Task.DoesNotExist:
+            return Response({'message': 'Task not found'}, status=status.HTTP_404_NOT_FOUND)
