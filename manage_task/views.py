c8bc28cbef7768adcd0fb7526e812aa30bc9cc63
NicDs <nikolasdurango@gmail.com>
2023-11-28 21:49:58 -0500

    created CRUD for Person model

--- manage_task/views.py
+++ manage_task/views.py
@@ -8,4 +8,30 @@ class PersonListAPIView(generics.ListAPIView):
 
     def get_queryset(self):
         return Person.objects.all()
+
+class PersonRetrieveAPIView(generics.RetrieveAPIView):
+    serializer_class = PersonSerializer
     
+    def get_queryset(self):
+        return Person.objects.all()
+
+class PersonCreateAPIView(generics.CreateAPIView):
+    serializer_class = PersonSerializer
+
+    def post(self, request):
+        serializer = PersonSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message': 'Person created successfully'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class PersonDestroyAPIView(generics.DestroyAPIView):
+    serializer_class = PersonSerializer
+    
+    def destroy(self, request, pk):
+        try:
+            person = Person.objects.get(pk=pk)
+            person.delete()
+            return Response({'message': 'Person deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
+        except Person.DoesNotExist:
+            return Response({'message': 'Person not found'}, status=status.HTTP_404_NOT_FOUND)
