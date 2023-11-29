2b2fa07ada37d65f3cc90f3c0d901a8e18726b27
NicDs <nikolasdurango@gmail.com>
2023-11-28 19:23:55 -0500

    created app for task management

---
+++ manage_task/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from models import Person, Task
+
+admin.site.register(Person)
+admin.site.register(Task)
---
+++ manage_task/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.core.exceptions import ValidationError
+
+def is_digit(value):
+    if not value.isdigit():
+        raise ValidationError('Must be number')
+
+class Person(models.Model):
+
+    class IdChoices(models.TextChoices):
+        CEDULA_CIUDADANIA = 'CC', 'Cedula de Ciudadania'
+        CEDULA_EXTRANJERA = 'CE', 'Cedula Extranjera'
+        TARJETA_IDENTIDAD = 'TI', 'Tarjeta de Identidad'
+
+    name = models.CharField('User name', max_length=100)
+    last_name = models.CharField('User last name', max_length=100)
+    document_type = models.CharField('Document type', max_length=2, choices=IdChoices.choices)
+    document_number = models.CharField('Document number', max_length=10, validators=[is_digit], unique=True)
+    email = models.EmailField('Email address', max_length=150, unique=True)
+
+    class Meta:
+        verbose_name = ('Person')
+        verbose_name_plural = ('Persons')
+
+    def __str__(self):
+        return self.name
+
+class Task(models.Model):
+    title = models.CharField('Task tittle', max_length=150)
+    description = models.TextField('Task description')
+    deadline_date = models.DateField('Task deadline date', auto_now=False, auto_now_add=False)
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = ('Task')
+        verbose_name_plural = ('Tasks')
+
+    def __str__(self):
+        return self.title
